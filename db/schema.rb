0b9717e76e3092dea94c5a7a0bcb1176fef185ce
smehsan <ehsanatwork@gmail.com>
2019-01-15 16:54:45 +0600

    Add more models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_14_192712) do
+ActiveRecord::Schema.define(version: 2019_01_15_105038) do
 
   create_table "comments", force: :cascade do |t|
     t.text "message"
@@ -23,12 +23,37 @@ ActiveRecord::Schema.define(version: 2019_01_14_192712) do
     t.index ["visitor_id"], name: "index_comments_on_visitor_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "visitor_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["visitor_id"], name: "index_messages_on_visitor_id"
+  end
+
   create_table "moderators", force: :cascade do |t|
     t.string "fullname"
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "notifiable_type"
+    t.integer "notifiable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable_type_and_notifiable_id"
+  end
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -47,6 +72,19 @@ ActiveRecord::Schema.define(version: 2019_01_14_192712) do
     t.boolean "under_maintanance"
     t.boolean "prevent_commenting"
     t.boolean "tag_visibility"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "visitors", force: :cascade do |t|
+    t.string "fullname"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
