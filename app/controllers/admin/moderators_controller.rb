19cac2b3296ed9b2536c75d7cbe51792f01179ee
smehsan <ehsanatwork@gmail.com>
2019-01-15 22:06:40 +0600

    update gemfile and give options to moderators

---
+++ app/controllers/admin/moderators_controller.rb
@@ -0,0 +1,27 @@
+class Admin::ModeratorsController < ApplicationController
+  def index
+    @moderators = Moderator.all
+  end
+
+  def edit
+    @moderator = Moderator.find(params[:id])
+  end
+
+  def update
+    @moderator = Moderator.find(params[:id])
+    if @moderator.update(moderator_params)
+      flash[:success] = "Moderator details updated successfully."
+      redirect_to admin_moderators_path
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+  def moderator_params
+    params.require(:moderator).permit(:fullname, :username, :password)
+  end
+
+
+end
--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -1,4 +1,16 @@
 class Moderator < ApplicationRecord
   has_secure_password
   has_many :posts
+
+  validates :fullname, 
+    presence: true, 
+    length: {minimum: 3, maximum: 100}
+  validates :username, 
+    presence: true, 
+    uniqueness: {case_sensitive: false}, 
+    length: {minimum: 3, maximum: 100},
+    format: {with: /\A[a-z0-9\+\-_\.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "Please enter a valid email address!"}
+  validates :password, 
+    presence: true, 
+    length: {minimum: 8, maximum: 100}
 end
